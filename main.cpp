3aec76239f97269db2f6b9a5a5961655deec572d
Corey Gray <csgray@alaska.edu>
2016-11-27 20:43:44 -0900

    Add comments and error-checking. Fix loops.

--- main.cpp
+++ main.cpp
@@ -24,69 +24,92 @@ using std::vector;
 int main()
 {
 	cout << "Welcome to Time Tracker." << endl;
-	string filename;
-
+	cout << "This program reads the last time the file was updated and the total elapsed time since the first read." << endl;
+	cout << "Then it updates the file with the current read time and total elapsed time." << endl;
+	cout << endl;
+	
 	while (true)
 	{
-		cout << "Which file are we opening? (Enter to exit)" << endl;
+		// User prompt for file or quit
+		cout << "Which file are we opening? Enter a blank line to quit." << endl;
+		string filename;
 		getline(cin, filename);
 		if (!cin)
 		{
 			cout << "Input error." << endl;
-			continue;
+			return 0;
 		}
+
+		// Quits
 		if (filename.empty())
 		{
 			cout << "Thanks, goodbye!" << endl;
+			return 0;
 		}
-		break;
-	}
 
-	ifstream infile(filename);
-	if (!infile)
-	{
-		cout << "Error" << endl;
-	}
-
-	vector <string> times;
-	while (true)
-	{
-		string line;
-		getline(infile, line);
+		// Opens file
+		ifstream infile(filename);
+		
+		// Error-checking (usually a nonexistent file)
 		if (!infile)
 		{
-			if (infile.eof())
-			{
-				break;
-			}
-			else
-			{
-				cout << "Error." << endl;
-				return 0;
-			}
+			cout << "File not found. Select a different file." << endl;
+			cout << endl;
+			continue;
 		}
-		times.push_back(line);
-	}
 
-	int oldt;
-	istringstream iss(times[0]);
-	iss >> oldt;
+		// Reads lines from file.
+		vector <string> times;
+		while (true)
+		{
+			string line;
+			getline(infile, line);
+			if (!infile)
+			{
+				if (infile.eof())
+				{
+					break;
+				}
+				else
+				{
+					cout << "Error reading file." << endl;
+					return 0;
+				}
+			}
+			times.push_back(line);
+		}
 
-	int elapsed;
-	istringstream iss2(times[1]);
-	iss2 >> elapsed;
+		// Closes file (otherwise it will error if opened later)
+		infile.close();
 
-	time_t now = time(NULL);
+		// Checks for an empty vector (from an empty file)
+		if (times.empty())
+		{
+			cout << "Empty file." << endl;
+			cout << endl;
+			continue;
+		}
 
-	long int diff = now - oldt;
+		// Converts file strings to ints for manipulation
+		// long long int required to not lose data while converting time_t to int
+		long long int oldt;
+		istringstream iss(times[0]);
+		iss >> oldt;
 
-	elapsed = elapsed + diff;
+		long long int elapsed;
+		istringstream iss2(times[1]);
+		iss2 >> elapsed;
 
-	cout << "The difference in times is " << diff << " seconds." << endl;
-	cout << "The total elapsed time is " << elapsed << " seconds." << endl;
+		// Calculates elapsed time
+		time_t now = time(NULL);
+		long long int diff = now - oldt;
+		elapsed = elapsed + diff;
 
-	while (true)
-	{
+		cout << endl;
+		cout << "The difference in times is " << diff << " seconds." << endl;
+		cout << "The total elapsed time is " << elapsed << " seconds." << endl;
+
+		// User-prompt for output file
 		cout << "Where are we storing the new time?" << endl;
 		getline(cin, filename);
 		if (!cin)
@@ -94,12 +117,21 @@ int main()
 			cout << "Input error." << endl;
 			continue;
 		}
-		break;
+
+		// Opens output file and checks for errors
+		ofstream outfile(filename);
+		if (!outfile)
+		{
+			cout << "File write error!" << endl;
+			return 0;
+		}
+
+		// Writes to file
+		outfile << now << endl;
+		outfile << elapsed << endl;
+		
+		cout << endl;
+		cout << "Write successful." << endl;
+		cout << endl;
 	}
-
-	ofstream outfile(filename);
-	outfile << now << endl;
-	outfile << elapsed << endl;
-	return 0;
-
 }
